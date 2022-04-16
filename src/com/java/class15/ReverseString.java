1bba27fcb9d2a62e8d1387e0b4dbb79821fa2f29
chiragkhimani <khimanichirag@gmail.com>
2022-04-16 07:30:01 +0530

    added space

--- src/com/java/class15/ReverseString.java
+++ src/com/java/class15/ReverseString.java
@@ -8,6 +8,7 @@ public class ReverseString {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
 
+        
         for(int i=str.length()-1; i>=0; i--){
             System.out.print(str.charAt(i));
         }
