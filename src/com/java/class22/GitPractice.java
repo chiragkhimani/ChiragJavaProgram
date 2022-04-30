47a1e06afc64be25db1118292a9502771fe1e929
SwatiPanda <swatisp2k@yahoo.com>
2022-04-29 22:05:18 -0400

    Login feature test

--- src/com/java/class22/GitPractice.java
+++ src/com/java/class22/GitPractice.java
@@ -2,9 +2,13 @@ package com.java.class22;
 
 public class GitPractice {
     public static void main(String[] args) {
+
         System.out.println("This is from Chirag's PC");
         System.out.println("gitKnowsMe was here");
         System.out.println("TestingMyFirstProgram");
         System.out.println("TestingHomeFeature-Swati");
+        System.out.println("Testing");
+        System.out.println("Login Feature Started");
+        System.out.println("Login feature completed till teh time manager is coming!! yay!");
     }
 }
