ab7edcafb8e39e81813f4edc679c295b7aee35a4
Eugene Mindrescu <singularity@Eugenes-M1.local>
2022-04-29 19:44:12 -0500

    I added a line

--- src/com/java/class22/GitPractice.java
+++ src/com/java/class22/GitPractice.java
@@ -3,5 +3,6 @@ package com.java.class22;
 public class GitPractice {
     public static void main(String[] args) {
         System.out.println("This is from Chirag's PC");
+        System.out.println("gitKnowsMe was here");
     }
 }
