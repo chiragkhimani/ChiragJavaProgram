41c4b91fcd3893329eaea9e5456eeb4a9046ba0e
beckyabildaeva <beckyabildaeva@Beckys-MBP.attlocal.net>
2022-04-29 20:15:25 -0500

    adding a line

--- src/com/java/class22/GitPractice.java
+++ src/com/java/class22/GitPractice.java
@@ -4,5 +4,6 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("This is from Chirag's PC");
         System.out.println("gitKnowsMe was here");
+        System.out.println("TestingMyFirstProgram");
     }
 }
