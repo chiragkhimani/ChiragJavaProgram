e6f3a9e559b4f553d5691bd79018049baffa37d8
yasemchyk <30556914+yasemchyk@users.noreply.github.com>
2022-05-06 21:53:47 -0400

    few spaces

--- src/com/java/class21/Homework4.java
+++ src/com/java/class21/Homework4.java
@@ -4,7 +4,7 @@ package com.java.class21;
 //  33
 //  555
 //  7777
-//  999
+//  99999
 
 public class Homework4 {
     public static void main(String[] args) {
@@ -12,7 +12,7 @@ public class Homework4 {
         for(int i=1; i<=9; i=i+2){
 
             for(int j=1; j<=(i+1)/2; j++){
-                    System.out.print(i);
+                    System.out.print( i );
             }
 
             System.out.println();
