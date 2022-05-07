e9f927509086f07c90a2e81be603882481132206
sagyn11 <sagynbek.taldybaev@mail.ru>
2022-05-07 08:08:33 +0600

    updated

--- src/com/java/class24/GitPractice.java
+++ src/com/java/class24/GitPractice.java
@@ -4,15 +4,7 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("Completed development");
         System.out.println("We're on chirag branch");
-<<<<<<< HEAD
-        System.out.println("Hello Git");
-=======
-<<<<<<< HEAD
-        System.out.println("This is Fernando's Text");
-=======
-        System.out.println("Oleg Did This");
->>>>>>> a67ff989c2efff5795544a07d1f99979283d70c5
->>>>>>> e38077bed3dd5c6b8bb2577019bdb2c729cb41a6
+
     }
 }
 
