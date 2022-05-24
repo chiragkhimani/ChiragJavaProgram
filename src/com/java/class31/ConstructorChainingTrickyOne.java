1c9d32b51fde90218fd5056ad165222c353d6d7e
chiragkhimani <khimanichirag@gmail.com>
2022-05-24 07:33:52 +0530

    Constructor Chaining

---
+++ src/com/java/class31/ConstructorChainingTrickyOne.java
@@ -0,0 +1,34 @@
+package com.java.class31;
+
+class A{
+    A(){
+        System.out.println("1");
+    }
+    void A(){
+        System.out.println("4");
+    }
+}
+class B extends A{
+    B(){
+        System.out.println("2");
+    }
+    void A(){
+        super.A();
+        System.out.println("5");
+    }
+}
+class C extends B{
+    C(){
+        System.out.println("3");
+    }
+    void A(){
+        super.A();
+        System.out.println("6");
+    }
+}
+public class ConstructorChainingTrickyOne{
+    public static void main(String []args){
+        C b = new C();
+        b.A();
+    }
+}
