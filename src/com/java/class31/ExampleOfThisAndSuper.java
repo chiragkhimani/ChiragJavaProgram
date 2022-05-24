d095263af2172c3ef43ad2513a661ff871443530
chiragkhimani <khimanichirag@gmail.com>
2022-05-24 05:59:50 +0530

    Constructor Chaining

---
+++ src/com/java/class31/ExampleOfThisAndSuper.java
@@ -0,0 +1,27 @@
+package com.java.class31;
+
+class GrandParent{
+    int a = 40;
+}
+
+class Parent extends GrandParent{
+    int a = 10; // instance variable
+}
+
+class Child extends Parent{
+    int a = 20; // instance variable
+
+    public void printData(){
+        int a = 50; // local variable
+        System.out.println(a); // 50
+        System.out.println(this.a); // 20
+        System.out.println(super.a); // 10
+    }
+}
+
+public class ExampleOfThisAndSuper {
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.printData();
+    }
+}
